f174bf94efe7d76725bff4e14b8f8d48345bf73f
Doug Anson <doug.anson@arm.com>
2016-11-03 22:11:42 -0500

    updates

--- start_instance.sh
+++ start_instance.sh
@@ -131,20 +131,6 @@ set_mqtt_port() {
    fi
 }  
 
-set_mbed_rest_api() {
-   MBED_REST_API="${@: -1}"
-   if [ "${MBED_REST_API}X" != "X" ]; then
-        DIR="mds/connector-bridge/conf"
-        FILE="gateway.properties"
-        cd /home/arm
-        sed -e "s/mbed_rest_api_goes_here/${MBED_REST_API}/g" ${DIR}/${FILE} 2>&1 1> ${DIR}/${FILE}.new
-        mv ${DIR}/${FILE} ${DIR}/${FILE}.mbed_rest_api
-        mv ${DIR}/${FILE}.new ${DIR}/${FILE}
-        chown arm.arm ${DIR}/${FILE}
-   fi
-
-}
-
 set_perms() {
   cd /home/arm
   chown -R arm.arm .
